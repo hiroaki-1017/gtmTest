e2b7d3ecd9b76e9a0d18e4d33afdae159211e721
山田寛顕 <yamadahiroaki@yamadahiroshikennoMacBook-Pro.local>
2023-01-06 11:50:32 +0900

    add dataLayer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 function buttonClick(){
     dataLayer.push({
-         "event" : "testCustomEvent" // イベント名
+         "event" : "testCustomEvent", // イベント名
+         "click_name" : "テストクリック",
+         "page_url" : "https://hoge.com"
     });    
+    console.log('test');
 }
