d6b8d8c64cc584be4ca0f174df0d81e698ee2bf8
山田寛顕 <yamadahiroaki@yamadahiroshikennoMacBook-Pro.local>
2023-01-10 19:08:55 +0900

    add event

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 function buttonClick(page_url){
     dataLayer.push({
          "event" : "stgCustomEvent", // イベント
-         "page_url" : page_url
+         "page_url" : page_url,
+         "obsession_value" : 'test'
     });    
     console.log('test');
     alert('カスタムイベントが送信されました')
