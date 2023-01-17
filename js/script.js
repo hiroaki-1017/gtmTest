c8ceac145736de86dc2f4cae09905b285e3f9a75
山田寛顕 <yamadahiroaki@yamadahiroshikennoMacBook-Pro.local>
2023-01-17 15:54:09 +0900

    イベント変更

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
-function buttonClick(page_url){
-    dataLayer.push({
-         "event" : "stgCustomEvent", // イベント
-         "page_url" : page_url,
-         "obsession_value" : 'test'
-    });    
-    console.log('test');
-    alert('カスタムイベントが送信されました')
+function buttonClick(page_url) {
+  dataLayer.push({
+    event: "stgCustomEvent", // イベント
+    click_name: page_url,
+    event_category: page_url,
+    elem_category: page_url,
+    page_url: page_url,
+    click_url: page_url,
+    area_value: page_url,
+    obsession_value: page_url,
+    skill_value: page_url,
+    position_value: page_url,
+    free_word: page_url,
+    article_title: page_url,
+    current_number: page_url,
+    page_title: page_url,
+    before_station: page_url,
+    after_station: page_url,
+    before_worklocation: page_url,
+    after_worklocation: page_url,
+    before_operatingperiod: page_url,
+    after_operatingperiod: page_url,
+    before_monthlyremuneration: page_url,
+    after_monthlyremuneration: page_url,
+    auth_status: page_url,
+  });
+  console.log("test");
+  alert("カスタムイベントが送信されました");
 }
