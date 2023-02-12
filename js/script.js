13d8b6637a005900ca0accd03cc8b71769bec465
山田寛顕 <yamadahiroaki@yamadahiroshikennoMacBook-Pro.local>
2023-02-13 00:27:58 +0900

    cpStgTest

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 function buttonClick(page_url) {
-  dataLayer.push({  
+  dataLayer.push({
     event: "myCustomEvent", // イベント名
     click_name: page_url,
     event_category: page_url,
     page_url: page_url,
     click_url: page_url,
-    page_title: page_url,
+    range_value: page_url,
     current_number: page_url,
+    detail_number: page_url,
+    detail_category: page_url,
+    page_title: page_url,
   });
   console.log("test");
   alert("カスタムイベントが送信されました");
