0319fa7bd5f2cbd0556604e934c0733302249c17
山田寛顕 <yamadahiroaki@yamadahiroshikennoMacBook-Pro.local>
2023-02-16 15:03:27 +0900

    stayTimeTest

--- js/script.js
+++ js/script.js
@@ -14,3 +14,22 @@ function buttonClick(page_url) {
   console.log("test");
   alert("カスタムイベントが送信されました");
 }
+
+
+window.onload = function(){
+  console.log("aaaaa");
+  let stay_time = 0;
+  function stayTime() {
+    stay_time += 5;
+    console.log(stay_time);
+  }
+  setInterval(stayTime, 5000);
+
+  window.addEventListener("beforeunload", () => {
+    console.log(stay_time);
+    dataLayer.push({
+      event: "stayTime", // イベント名
+      stay_time: stay_time,
+    });
+  });
+}
